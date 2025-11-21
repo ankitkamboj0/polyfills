39271845254b1ff4780d0408667af85cbed6df44
Ankit kamboj <ankitkamboj@Ankits-MacBook-Pro.local>
2025-11-21 15:23:01 +0530

    added class based promises

--- promise.js
+++ promise.js
@@ -64,3 +64,61 @@ const pro1 = myPromise.resolve(10);
 
 
 pro1.then((res)=>console.log("custom",res));
+
+class MyPromiseClass {
+    constructor(executer){
+        this.onResolve = null
+        this.onReject = null
+        this.value=null
+        this.isFullfilled=false
+        this.isRejected=false
+        this.called=false
+
+        const resolve = (val)=>{
+            console.log("inside resolve")
+            this.value=val;
+            this.isFullfilled=true;
+            if(typeof this.onResolve === 'function'){
+                this.onResolve(val);
+                this.called=true;
+            }
+        }
+        const reject = (val) =>{
+            this.value=val;
+            this.isRejected=true;
+            if(typeof this.onReject === 'function'){
+                this.onReject(val);
+                this.called=true;
+            }
+        }
+        try {
+            executer(resolve,reject);
+        } catch (error) {
+            reject(error);
+        }
+    }
+    then(callback){
+        this.onResolve = callback;
+        if(this.isFullfilled && !this.called){
+            this.onResolve(this.value)
+            this.called=true;
+        }
+        return this
+    }
+    catch(callback){
+        this.onReject=callback;
+        if(this.isRejected && !this.called){
+            this.onReject(this.value);
+            this.called=true
+        }
+        return this;
+    }
+}
+
+let promise = new MyPromiseClass((res,rej)=>{
+    // setTimeout(()=>{
+        rej("test class promise")
+    // },1000)
+});
+
+promise.then((res)=>console.log(res)).catch((err)=>console.log(err));
